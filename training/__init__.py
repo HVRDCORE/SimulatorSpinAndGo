55ba01d80ce7a89b5cafa6db723e83b1c3209999
HVRDCORE <122425777+HVRDCORE@users.noreply.github.com>
2025-04-18 14:53:22 +0700

    Delete training directory

--- training/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Training package for the poker simulator.
-
-This package provides utilities for training poker agents using various
-machine learning algorithms, including distributed training capabilities.
-"""
