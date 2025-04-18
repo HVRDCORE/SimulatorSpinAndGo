bd8a451641bab7eac3b98ddd373bac322a3f1a34
HVRDCORE <122425777+HVRDCORE@users.noreply.github.com>
2025-04-18 14:52:15 +0700

    Delete config directory

--- config/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Configuration package for the poker simulator.
-
-This package provides tools for configuring the poker simulator, including
-loading configurations from files, command-line arguments, and environment
-variables.
-"""
